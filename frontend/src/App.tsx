7ee6a2e859dde677877f3ea85b9a913929418e66
vpivovarov <pivovarov.main@gmail.com>
2024-02-05 00:28:56 +0200

    [AW-0002] create: response on a screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const timerId = setTimeout(async () => {
+      const response = await fetch("/ping", { method: "get" });
+
+      if (!response.ok) {
+        return;
+      }
+
+      const text = await response.text();
+      setText(text);
+    }, 500);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {text ? (
+          <p>Response from the server is {text}.</p>
+        ) : (
+          <span>loading...</span>
+        )}
         <a
           className="App-link"
           href="https://reactjs.org"
